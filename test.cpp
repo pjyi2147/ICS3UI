4e703e9e012989cb4e4f5350d0c6c6eb6d9dfe5d
Jeongseop Yi <yjs990427@gmail.com>
2017-03-27 12:11:49 -0400

    test

--- test.cpp
+++ test.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
-#include <bitset>
 
 using namespace std;
-int main() {
 
+int main(void) {
+    int *ptr, arr[3], *ptr2, i;
+    arr[0] = 3;
+    ptr = arr;
+    ptr2 = ptr + 1;
+    i = ptr2 - ptr;
+    cout << ptr << " " << ptr2 << " " << i << endl;
+    return 0;
 }
